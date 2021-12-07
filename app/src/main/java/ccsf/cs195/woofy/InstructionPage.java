c72bb98cb75a7f58c0449589fe07cc989d1113d8
jaden-letsgo <tl.li@hotmail.com>
2021-12-07 17:09:55 -0500

    Created instructionPage

--- app/src/main/java/ccsf/cs195/woofy/InstructionPage.java
+++ app/src/main/java/ccsf/cs195/woofy/InstructionPage.java
@@ -1,6 +1,6 @@
 package ccsf.cs195.woofy;
 /*
-Program Note: This class is the backend of landing page
+Program Note: This class is the backend of instruction page
  */
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,28 +8,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class LandingPage extends AppCompatActivity {
+public class InstructionPage extends AppCompatActivity {
 
-    //Button to move to next activity
     Button nextActivityButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_instruction_page);
 
-        nextActivityButton = (Button)findViewById(R.id.button_begin);
+        nextActivityButton = (Button)findViewById(R.id.next_button);
 
         nextActivityButton.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v)
             {
-                Intent intent = new Intent(LandingPage.this, InstructionPage.class);
+                Intent intent = new Intent(InstructionPage.this, QuestionPage.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
+
     }
 }
